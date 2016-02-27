f02b801857506b345c62029498d3291b182ab373
mpatel <mpatel@shutterstock.com>
2016-02-26 23:38:33 -0500

    fix text color toggle

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $('#grayButton').click(function(){
 	$('#container').css ('background-color',"gray");
+	$('#text_color_toggle').css('color',"white");
 });
 
 $('#whiteButton').click(function(){
 	$('#container').css ('background-color',"white");
+	$('#text_color_toggle').css('color',"black");
 });
